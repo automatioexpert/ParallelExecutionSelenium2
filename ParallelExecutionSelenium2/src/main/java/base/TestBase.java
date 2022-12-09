2cfc680c61977846c2eac467ff98e705d63f32c9
Valmiki Vishwakarma <you@example.com>
2022-12-09 15:54:47 +0530

    First commit.

---
+++ ParallelExecutionSelenium2/src/main/java/base/TestBase.java
@@ -0,0 +1,17 @@
+package base;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+	@BeforeSuite
+	public void setUp() {
+
+	}
+
+	@AfterSuite
+	public void tearDown() {
+
+	}
+}
